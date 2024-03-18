e246a6f0c6f9ffe416e84d246e6447c3bf843a4e
labpass123 <163888513+labpass123@users.noreply.github.com>
2024-03-19 00:09:16 +0530

    Update 3-c.py

--- 3-c.py
+++ 3-c.py
@@ -1,34 +1,31 @@
 import numpy as np
 
-# Sorting
-# Sort along the first axis
+
 a = np.array([[12, 15], [10, 1]])
 sorted_first_axis = np.sort(a, axis=0)
 print("Sorted along first axis:\n", sorted_first_axis)
 
-# Sort along the last axis
+
 a = np.array([[10, 15], [12, 1]])
 sorted_last_axis = np.sort(a, axis=-1)
 print("\nSorted along last axis:\n", sorted_last_axis)
 
-# Sort along none axis (flattened)
 a = np.array([[12, 15], [10, 1]])
 sorted_none_axis = np.sort(a, axis=None)
 print("\nSorted along none axis (flattened):\n", sorted_none_axis)
 
-# Searching
+
 array = np.arange(12).reshape(3, 4)
 print("\nInput ARRAY:\n", array)
 
-# Maximum element without specifying axis
+
 print("\nMax element:", np.max(array))
 
-# Indices of the max element along axis 0 and axis 1
+
 print("Indices of Max element along axis 0:", np.argmax(array, axis=0))
 print("Indices of Max element along axis 1:", np.argmax(array, axis=1))
 
-# Counting
-# Counting the number of non-zero values
+
 a = np.count_nonzero([[0, 1, 7, 0, 0], [3, 0, 0, 2, 19]])
 b = np.count_nonzero([[0, 1, 7, 0, 0], [3, 0, 0, 0, 19]])
 
