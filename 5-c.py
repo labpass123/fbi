4d7ce9a3a4ed15ff3e4edb642a3ce906c2f8fb07
labpass123 <163888513+labpass123@users.noreply.github.com>
2024-05-09 00:42:58 +0530

    Update 5-c.py

--- 5-c.py
+++ 5-c.py
@@ -1,36 +1,17 @@
-#5-c: Write a program that demonstrates the row selection, row addition, and row deletion.
-
-# Row Selection:
 import pandas as pd
 
-# making data frame from csv file
-data = pd.read_csv("C:/Users/M.V.B.Chandrasekhar/Desktop/FAI/nba.csv", index_col ="Name")
+# Row Selection
+data = pd.read_csv("C:\\Users\\danda\\OneDrive\\Documents\\Desktop\\CSM A\\nba.csv", index_col="Name")
+first_row = data.loc["Avery Bradley"]
+second_row = data.loc["R.J. Hunter"]
+print("First row:\n", first_row, "\n\nSecond row:\n", second_row)
 
-# retrieving row by loc method
-first = data.loc["Avery Bradley"]
-second = data.loc["R.J. Hunter"]
-print(first, "\n\n\n", second)
+# Row Addition
+df = pd.read_csv("C:\\Users\\danda\\OneDrive\\Documents\\Desktop\\CSM A\\nba.csv", index_col="Name")
+new_row = pd.DataFrame({'Team': 'Engineering', 'Number': 3, 'Position': 'PG', 'Age': 33, 'Height': '6-2', 'Weight': 189, 'College': 'AITAM', 'Salary': 99999}, index=['Aditya'])
+df = pd.concat([new_row, df]).reset_index(drop=True)
+print("DataFrame after adding a new row:\n", df.head())
 
-# Row Addition: In Order to add a Row in Pandas DataFrame, concat the old dataframe with new one.
-import pandas as pd
-	
-# making data frame
-df = pd.read_csv("C:/Users/M.V.B.Chandrasekhar/Desktop/FAI/nba.csv", index_col ="Name")
-df.head(10)
-
-new_row = pd.DataFrame({'Name':'Aditya', 'Team':'Engineering', 'Number':3,'Position':'PG', 'Age':33, 'Height':'6-2','Weight':189, 'College':'AITAM', 'Salary':99999},index =[0])
-# simply concatenate both dataframes
-df = pd.concat([new_row, df]).reset_index(drop = True)
-df.head(5)
-
-# Row Deletion: In Order to delete a row in Pandas DataFrame, use the drop() method. Rows is deleted by dropping Rows by index label.
-import pandas as pd
-
-# making data frame from csv file
-data = pd.read_csv("C:/Users/M.V.B.Chandrasekhar/Desktop/FAI/nba.csv", index_col ="Name" )
-
-# dropping passed values
-data.drop(["Avery Bradley", "John Holland", "R.J. Hunter","R.J. Hunter"], inplace = True)
-
-# display
-print(data)
+# Row Deletion
+data.drop(["Avery Bradley", "John Holland", "R.J. Hunter"], inplace=True)
+print("DataFrame after deleting specified rows:\n", data)
