4cdffc67fe6ecf190d00649b8285ae61712233e5
labpass123 <163888513+labpass123@users.noreply.github.com>
2024-05-09 00:46:35 +0530

    Update 6-a.py

--- 6-a.py
+++ 6-a.py
@@ -1,36 +1,13 @@
 #6-a:Write a program which use Pandas inbuilt visualization to plot following graphs: i) Bar plots ii) Histograms  iii) Line plots  iv) Scatter plots
-
 import numpy as np
 import pandas as pd
-
-# There are some fake data csv files you can read in as dataframes
-df1 = pd.read_csv('df1', index_col = 0)
-df2 = pd.read_csv('df2')
-
-df1['A'].hist()
-
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
-df1['A'].hist()
-
-#look like this after calling bmh style.
-plt.style.use('bmh')
-df1['A'].hist()
-
-#Plots look like this after calling dark_background style.
-plt.style.use('dark_background')
-df1['A'].hist()
-
-#Bar lot
-df2.plot.bar()
-df2.plot.bar(stacked = True)
-
-df1['A'].plot.hist(bins = 50)
-
-#Line plot
-df1.plot.line(x = df1.index, y ='B', figsize =(12, 3), lw = 1)
-
-#Scatter Plot
-df1.plot.scatter(x ='A', y ='B')
-df1.plot.scatter(x ='A', y ='B', c ='C', cmap ='coolwarm')
-df1.plot.scatter(x ='A', y ='B', s = df1['C']*200)
+data={'category':['A','B','C','D','E'],'value1':[10,20,15,25,30], 'value2':[5,15,10,20,25]}
+df=pd.DataFrame(data)
+df.plot(kind='bar',x='category',y=['value1','value2'],title='bar plot')
+plt.show()
+df['value1'].plot(kind='hist',bins=10,title='histogram')
+plt.show()
+df.plot(kind='line',x='category',y=['value1','value2'],marker='o',title='line plot')
+plt.show()
+df.plot(kind='scatter',x='value1',y='value2',title='scatter plot')
