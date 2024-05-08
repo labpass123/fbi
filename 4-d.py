f80e4e90f71d0ac089808f9c754867dcc71f46a2
labpass123 <163888513+labpass123@users.noreply.github.com>
2024-05-09 00:39:29 +0530

    Update 4-d.py

--- 4-d.py
+++ 4-d.py
@@ -1,49 +1,25 @@
 #4-d: Write a program which makes use of the following Pandas methods i) describe() ii) head() iii) tail() iv) info()
-
 import pandas as pd
 import numpy as np
-import re  # importing regex module
 
-#Create a series with 4 random numbers
+# Generating a random series of data
 s = pd.Series(np.random.randn(4))
+
 print("The original series is:")
 print(s)
-
 print("\n")
 
 print("The first two rows of the data series:")
 print(s.head(2))
 print("\n")
 
-print ("The last two rows of the data series:")
+print("The last two rows of the data series:")
 print(s.tail(2))
 print("\n")
 
-print(s.describe());
-print("\n")
-	
-# making data frame
-data = pd.read_csv("nba.csv")
-	
-# removing null values to avoid errors
-data.dropna(inplace = True)
-
-# percentile list
-perc =[.20, .40, .60, .80]
-
-# list of dtypes to include
-include =['object', 'float', 'int']
-
-# calling describe method
-desc = data.describe(percentiles = perc, include = include)
-
-# display
-print(desc)
-
+print("Statistical summary of the data series:")
+print(s.describe())
 print("\n")
 
-# calling describe method
-desc = data["Name"].describe()
-
-# display
-print(desc)
+print("Information about the data series:")
+print(s.info())
